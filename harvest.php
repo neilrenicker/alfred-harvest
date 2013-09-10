21129a4173304fcfe0ab0eeef097237ac7d785f6
Neil Renicker <neilrenicker@gmail.com>
2013-09-09 23:18:08 -0400

    refactor: set cache on initial workflow setup

--- harvest.php
+++ harvest.php
@@ -2,32 +2,7 @@
 
   $query = trim($argv[1]);
 
-  require('auth.php');
-  $url = "https://$subdomain.harvestapp.com/daily";
-
-  $headers = array (
-    "Content-type: application/json",
-    "Accept: application/json",
-    "Authorization: Basic " . base64_encode($credentials)
-  );
-
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_VERBOSE, 0);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $response = curl_exec($ch);
-
-  if (curl_errno($ch)) {
-    print "Error: " . curl_error($ch);
-  } else {
-    curl_close($ch);
-  }
-
-  $fp = fopen('projects.txt', 'w');
-  fwrite($fp, $response);
-  fclose($fp);
+  require('getdaily.php');
 
   $data = json_decode($response, true);
   $xml = "<?xml version=\"1.0\"?>\n<items>\n";
