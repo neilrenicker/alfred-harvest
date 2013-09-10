0164c86e7940c8bc77e86dec26a4e29c60cafc5a
Neil Renicker <neilrenicker@gmail.com>
2013-09-10 00:11:51 -0400

    feat: add filtering for toggle timer workflow

--- harvest.php
+++ harvest.php
@@ -9,7 +9,7 @@
     $data = json_decode($response, true);
     $xml = "<?xml version=\"1.0\"?>\n<items>\n";
 
-    foreach ($data["day_entries"] as $day_entry){
+    foreach ($data["day_entries"] as $day_entry) {
       $project = htmlspecialchars($day_entry["project"]);
       $task    = htmlspecialchars($day_entry["task"]);
       $client  = htmlspecialchars($day_entry["client"]);
@@ -17,42 +17,6 @@
       $active  = $day_entry["timer_started_at"];
       $id      = $day_entry["id"];
 
-      if ( $active ) {
-        $xml .= "<item uid=\"harvestcurrent-$id\" arg=\"$id\">\n";
-      } else {
-        $xml .= "<item arg=\"$id\">\n";
-      }
-
-      $xml .= "<title>$hours hours – $project</title>\n";
-      $xml .= "<subtitle>$client, $task></subtitle>\n";
-
-      if ( $active ) {
-        $xml .= "<icon>stop.png</icon>\n";
-      } else {
-        $xml .= "<icon>go.png</icon>\n";
-      }
-
-      $xml .= "</item>\n";
-    }
-
-    $xml .= "</items>";
-    echo $xml;
-
-  } elseif ( stripos($project, $query) !== false ) {
-
-    $data_raw = file_get_contents('projects.txt');
-    $data = json_decode($data_raw, true);
-
-    $xml = "<?xml version=\"1.0\"?>\n<items>\n";
-
-    foreach ($data["day_entries"] as $day_entry){
-      $project = htmlspecialchars($day_entry["project"]);
-      $task    = htmlspecialchars($day_entry["task"]);
-      $client  = htmlspecialchars($day_entry["client"]);
-      $hours   = $day_entry["hours"];
-      $active  = $day_entry["timer_started_at"];
-      $id      = $day_entry["id"];
-    
       if ( $active ) {
         $xml .= "<item uid=\"harvestcurrent-$id\" arg=\"$id\">\n";
       } else {
@@ -71,6 +35,45 @@
       $xml .= "</item>\n";
     }
 
+    $xml .= "</items>";
+    echo $xml;
+
+  } else {
+
+    $data_raw = file_get_contents('projects.txt');
+    $data = json_decode($data_raw, true);
+
+    $xml = "<?xml version=\"1.0\"?>\n<items>\n";
+
+    foreach ($data["day_entries"] as $day_entry){
+      $project = htmlspecialchars($day_entry["project"]);
+      $task    = htmlspecialchars($day_entry["task"]);
+      $client  = htmlspecialchars($day_entry["client"]);
+      $hours   = $day_entry["hours"];
+      $active  = $day_entry["timer_started_at"];
+      $id      = $day_entry["id"];
+
+      if ( stripos($project, $query) !== false ) {
+    
+        if ( $active ) {
+          $xml .= "<item uid=\"harvestcurrent-$id\" arg=\"$id\">\n";
+        } else {
+          $xml .= "<item arg=\"$id\">\n";
+        }
+
+        $xml .= "<title>$hours hours – $project</title>\n";
+        $xml .= "<subtitle>$client, $task</subtitle>\n";
+
+        if ( $active ) {
+          $xml .= "<icon>stop.png</icon>\n";
+        } else {
+          $xml .= "<icon>go.png</icon>\n";
+        }
+
+        $xml .= "</item>\n";
+      }
+    }
+    
     $xml .= "</items>";
     echo $xml;
   }
