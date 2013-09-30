b4ac310e5a633e43521bed06b2c9fd7835100ae9
Neil Renicker <neilrenicker@gmail.com>
2013-09-29 21:14:24 -0400

    wip: progress on notes feature

---
+++ note.php
@@ -0,0 +1,86 @@
+<?php
+
+  $query = trim($argv[1]);
+  $dir = "../../../Workflow Data/com.neilrenicker.harvest/";
+
+  if ( substr_count( $query, '→' ) == 0 ):
+
+    require('getdaily.php');
+
+    $data = json_decode($response, true);
+    $xml = "<?xml version=\"1.0\"?>\n<items>\n";
+
+    foreach ($data["day_entries"] as $day_entry) {
+      $project = htmlspecialchars($day_entry["project"]);
+      $task    = htmlspecialchars($day_entry["task"]);
+      $client  = htmlspecialchars($day_entry["client"]);
+      $hours   = $day_entry["hours"];
+      $active  = $day_entry["timer_started_at"];
+      $id      = $day_entry["id"];
+
+      if ( $active ) {
+        $xml .= "<item valid=\"no\" uid=\"harvestcurrent\" autocomplete=\" $project ($task) → \">\n";
+      } else {
+        $xml .= "<item valid=\"no\" autocomplete=\" $project ($task) → \">\n";
+      }
+
+      $xml .= "<title>$hours hours – $project</title>\n";
+      $xml .= "<subtitle>$client, $task</subtitle>\n";
+
+      if ( $active ) {
+        $xml .= "<icon>stop.png</icon>\n";
+      } else {
+        $xml .= "<icon>go.png</icon>\n";
+      }
+
+      $xml .= "</item>\n";
+    }
+
+    $xml .= "</items>";
+    echo $xml;
+
+  elseif ( substr_count( $query, '→' ) == 1 ):
+
+    $strings = explode( " →", $query);
+    $project_name = $strings[0];
+    $newQuery = $strings[1];
+    $data_raw = file_get_contents($dir . 'projects.json');
+    $data = json_decode($data_raw, true);
+
+    foreach ( $data["projects"] as $project ){
+
+      if ( $project["name"] == $project_name ) {
+        $project_tasks = $project["tasks"];
+        $project_name = $project["name"];
+        $project_id = $project["id"];
+        $project_name_encoded = str_replace(" ", "_", htmlspecialchars($project_name));
+      }
+    }
+
+    $xml = "<?xml version=\"1.0\"?>\n<items>\n";
+
+    foreach ($project_tasks as $task){
+      $task_name = htmlspecialchars($task["name"]);
+      $task_id = $task["id"];
+
+      if ( !$newQuery ) {
+        $xml .= "<item arg=\"$project_id|$task_id|$project_name_encoded\">\n";
+        $xml .= "<title>$task_name</title>\n";
+        $xml .= "<subtitle>Start this task</subtitle>\n";
+        $xml .= "<icon>go.png</icon>\n";
+        $xml .= "</item>\n";
+      } elseif ( stripos(" " . $task_name, $newQuery) !== false ) {
+        $xml .= "<item arg=\"$project_id|$task_id|$project_name_encoded\">\n";
+        $xml .= "<title>$task_name</title>\n";
+        $xml .= "<subtitle>Start this task</subtitle>\n";
+        $xml .= "<icon>go.png</icon>\n";
+        $xml .= "</item>\n";
+      }
+    }
+
+    $xml .= "</items>";
+    echo $xml;
+
+  endif;
+
+?>
