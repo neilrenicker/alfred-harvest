a4b9aa666827686de1f389a2256563b831389ed5
Neil Renicker <neilrenicker@gmail.com>
2013-10-04 22:29:48 -0400

    feat: add option on toggle to start new timer if none today

--- commands/post_toggle.php
+++ commands/post_toggle.php
@@ -2,35 +2,37 @@
 
   $query = trim($argv[1]);
 
-  require('auth.php');
-  $url = "https://$subdomain.harvestapp.com/daily/timer/$query";
+  if ($query) {
+    require('auth.php');
+    $url = "https://$subdomain.harvestapp.com/daily/timer/$query";
 
-  $headers = array (
-    "Content-type: application/json",
-    "Accept: application/json",
-    "Authorization: Basic " . base64_encode($credentials)
-  );
+    $headers = array (
+      "Content-type: application/json",
+      "Accept: application/json",
+      "Authorization: Basic " . base64_encode($credentials)
+    );
 
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_VERBOSE, 0);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $data_raw = curl_exec($ch);
-  curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data_raw = curl_exec($ch);
+    curl_close($ch);
 
-  $data = json_decode($data_raw, true);
+    $data = json_decode($data_raw, true);
 
-  $was_started = $data["timer_started_at"];
-  $project = $data["project"];
+    $was_started = $data["timer_started_at"];
+    $project = $data["project"];
 
-  if ( $was_started ) {
-    $query = "Started —" . " " . $project;
-  } else {
-    $query = "Stopped —" . " " . $project;
+    if ( $was_started ) {
+      $query = "Started —" . " " . $project;
+    } else {
+      $query = "Stopped —" . " " . $project;
+    }
+
+    echo $query;
   }
-
-  echo $query;
 
 ?>
--- commands/toggle.php
+++ commands/toggle.php
@@ -10,58 +10,16 @@
     $data = json_decode($response, true);
     $xml = "<?xml version=\"1.0\"?>\n<items>\n";
 
-    foreach ($data["day_entries"] as $day_entry) {
-      $project = htmlspecialchars($day_entry["project"]);
-      $task    = htmlspecialchars($day_entry["task"]);
-      $client  = htmlspecialchars($day_entry["client"]);
-      $notes   = htmlspecialchars($day_entry["notes"]);
-      $hours   = $day_entry["hours"];
-      $active  = $day_entry["timer_started_at"];
-      $id      = $day_entry["id"];
+    if ($data["day_entries"]) {
+      foreach ($data["day_entries"] as $day_entry) {
+        $project = htmlspecialchars($day_entry["project"]);
+        $task    = htmlspecialchars($day_entry["task"]);
+        $client  = htmlspecialchars($day_entry["client"]);
+        $notes   = htmlspecialchars($day_entry["notes"]);
+        $hours   = $day_entry["hours"];
+        $active  = $day_entry["timer_started_at"];
+        $id      = $day_entry["id"];
 
-      if ( $active ) {
-        $xml .= "<item uid=\"harvesttoggle-$id\" arg=\"$id\">\n";
-      } else {
-        $xml .= "<item arg=\"$id\">\n";
-      }
-
-      $xml .= "<title>$hours hours – $project</title>\n";
-
-      if ( $notes ) {
-        $xml .= "<subtitle>$client, $task – \"$notes\"</subtitle>\n";
-      } else {
-        $xml .= "<subtitle>$client, $task</subtitle>\n";
-      }
-      if ( $active ) {
-        $xml .= "<icon>icons/stop.png</icon>\n";
-      } else {
-        $xml .= "<icon>icons/go.png</icon>\n";
-      }
-
-      $xml .= "</item>\n";
-    }
-
-    $xml .= "</items>";
-    echo $xml;
-
-  } else {
-
-    $data_raw = file_get_contents($dir . 'projects.json');
-    $data = json_decode($data_raw, true);
-
-    $xml = "<?xml version=\"1.0\"?>\n<items>\n";
-
-    foreach ($data["day_entries"] as $day_entry){
-      $project = htmlspecialchars($day_entry["project"]);
-      $task    = htmlspecialchars($day_entry["task"]);
-      $client  = htmlspecialchars($day_entry["client"]);
-      $notes   = htmlspecialchars($day_entry["notes"]);
-      $hours   = $day_entry["hours"];
-      $active  = $day_entry["timer_started_at"];
-      $id      = $day_entry["id"];
-
-      if ( stripos($project . $task . $client . $notes, $query) !== false ) {
-    
         if ( $active ) {
           $xml .= "<item uid=\"harvesttoggle-$id\" arg=\"$id\">\n";
         } else {
@@ -75,7 +33,6 @@
         } else {
           $xml .= "<subtitle>$client, $task</subtitle>\n";
         }
-
         if ( $active ) {
           $xml .= "<icon>icons/stop.png</icon>\n";
         } else {
@@ -84,6 +41,66 @@
 
         $xml .= "</item>\n";
       }
+      
+    } else {
+      $xml .= "<item>\n";
+      $xml .= "<title>No timers yet today. Start one?</title>\n";
+      $xml .= "<subtitle>Press 'Enter' to select a new timer...</subtitle>\n";
+      $xml .= "<icon>icons/icon.png</icon>\n";
+      $xml .= "</item>\n";
+    }
+
+    $xml .= "</items>";
+    echo $xml;
+
+  } else {
+
+    $data_raw = file_get_contents($dir . 'projects.json');
+    $data = json_decode($data_raw, true);
+
+    $xml = "<?xml version=\"1.0\"?>\n<items>\n";
+
+    if ($data["day_entries"]) {
+      foreach ($data["day_entries"] as $day_entry){
+        $project = htmlspecialchars($day_entry["project"]);
+        $task    = htmlspecialchars($day_entry["task"]);
+        $client  = htmlspecialchars($day_entry["client"]);
+        $notes   = htmlspecialchars($day_entry["notes"]);
+        $hours   = $day_entry["hours"];
+        $active  = $day_entry["timer_started_at"];
+        $id      = $day_entry["id"];
+
+        if ( stripos($project . $task . $client . $notes, $query) !== false ) {
+      
+          if ( $active ) {
+            $xml .= "<item uid=\"harvesttoggle-$id\" arg=\"$id\">\n";
+          } else {
+            $xml .= "<item arg=\"$id\">\n";
+          }
+
+          $xml .= "<title>$hours hours – $project</title>\n";
+
+          if ( $notes ) {
+            $xml .= "<subtitle>$client, $task – \"$notes\"</subtitle>\n";
+          } else {
+            $xml .= "<subtitle>$client, $task</subtitle>\n";
+          }
+
+          if ( $active ) {
+            $xml .= "<icon>icons/stop.png</icon>\n";
+          } else {
+            $xml .= "<icon>icons/go.png</icon>\n";
+          }
+
+          $xml .= "</item>\n";
+        }
+      }
+    } else {
+      $xml .= "<item>\n";
+      $xml .= "<title>No timers yet today. Start one?</title>\n";
+      $xml .= "<subtitle>Press 'Enter' to select a new timer...</subtitle>\n";
+      $xml .= "<icon>icons/icon.png</icon>\n";
+      $xml .= "</item>\n";
     }
     
     $xml .= "</items>";
