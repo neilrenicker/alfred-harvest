921a6c20e8d0d72a19c438960e5a4755b3119ccc
Michael Ledford <michael@ledford.cc>
2014-04-25 10:59:05 -0400

    Migrate passwords into the keychain without user intervention!

--- commands/auth.php
+++ commands/auth.php
@@ -8,6 +8,16 @@
   $strings = explode( "\n", $data);
   $subdomain = $strings[0];
   $email = $strings[1];
+
+  // Migrate to using the keychain
+  if (count($strings) == 3) {
+      $id = "$subdomain\n$email";
+      $password = $strings[2];
+      file_put_contents( $dir . 'id.txt', $id );
+      shell_exec('/usr/bin/security delete-generic-password -s alfred-harvest-workflow > /dev/null 2>&1');
+      shell_exec('/usr/bin/security -v add-generic-password -a alfred-harvest -l alfred-harvest -s alfred-harvest-workflow -w \'' . $password . '\' > /dev/null 2>&1');
+  }
+  
   $password = shell_exec('/usr/bin/security find-generic-password -s alfred-harvest-workflow -w');
   $password = preg_replace('/(.+)\n$/', '${1}', $password);
   $credentials = $email . ":" . $password;
