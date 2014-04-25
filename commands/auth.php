fba09aa4169afb359dcd71c1feb7752aa6dc8025
Michael Ledford <michael@ledford.cc>
2014-04-24 21:49:06 -0400

    Now stores the password in the keychain instead of unencrypted on the file system.

--- commands/auth.php
+++ commands/auth.php
@@ -8,6 +8,7 @@
   $strings = explode( "\n", $data);
   $subdomain = $strings[0];
   $email = $strings[1];
-  $password = $strings[2];
+  $password = shell_exec('/usr/bin/security find-generic-password -s alfred-harvest-workflow -w');
+  $password = preg_replace('/(.+)\n$/', '${1}', $password);
   $credentials = $email . ":" . $password;
 ?>
