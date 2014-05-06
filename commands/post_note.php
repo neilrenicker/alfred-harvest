d00ea5fee80ebca05bcda9ffa8661662a4496f63
Eric Boehs <ericboehs@gmail.com>
2014-05-06 15:39:45 -0500

    Requery for note before appending to entry

--- commands/post_note.php
+++ commands/post_note.php
@@ -11,6 +11,25 @@
     $entry_project_id = $strings[3];
     $entry_task_id = $strings[4];
 
+    require('get_daily.php');
+    $data = json_decode($response, true);
+     if ($data["day_entries"]) {
+      foreach ( $data["day_entries"] as $entry ){
+
+        if ( $entry["id"] == $entry_task_id ) {
+          $existing_entry_notes = $entry["notes"];
+          $existing_entry_hours = $entry["hours"];
+          $existing_entry_spent_at = $entry["spent_at"];
+          $existing_entry_project_id = $entry["project_id"];
+          $existing_entry_task_id = $task_id;
+          $existing_entry_project_name = htmlspecialchars($entry["project"]);
+          $existing_entry_task = htmlspecialchars($entry["task"]);
+          $existing_active  = $entry["timer_started_at"];
+        }
+      }
+    }
+
+
     $default_tz = "UTC";
     $tz_file = readlink("/etc/localtime");
     $pos = strpos($tz_file, "zoneinfo");
@@ -28,7 +47,7 @@
     $url = "https://$subdomain.harvestapp.com/daily/update/$entry_task_id";
 
     // build xml_data to post to Harvest API:
-    $xml_data = "<request> <notes>$entry_notes</notes> <hours>$entry_hours</hours> <spent_at type=\"date\">$entry_spent_at</spent_at> <project_id>$entry_project_id</project_id> <task_id>$entry_task_id</task_id> </request>";
+    $xml_data = "<request> <notes>$existing_entry_notes\n$entry_notes</notes> <hours>$existing_entry_hours</hours> <spent_at type=\"date\">$entry_spent_at</spent_at> <project_id>$entry_project_id</project_id> <task_id>$entry_task_id</task_id> </request>";
 
     $headers = array (
       "Content-type: application/xml",
